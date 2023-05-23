10e386e0c1d01dca37f7928af592ecfd531ce6b4
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-23 18:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 function changeText() {
-  
-  var status = document.getElementById("status");
+    var status = document.getElementById("status");
 
-  
+
   status.innerHTML = "<h1>Entered Metaverse</h1>";
+ 
 }
